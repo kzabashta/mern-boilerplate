1385c3f9aa724966d7812cd1a9df8933b1e97da6
Kosta <kosta.zabashta@gmail.com>
2015-12-26 22:36:46 -0500

    Further refactoring

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-app.use(express.static('client'));
+app.use('/bower_components',  express.static(__dirname + '/dist/bower_components'));
+app.use('/dist/src',  express.static(__dirname + '/dist/src'));
+app.use('/build',  express.static(__dirname + '/dist/build'));
+
 app.get('/', function(req, res) {
-    res.sendfile('client/index.html');
+    res.sendfile('dist/index.html');
 });
 
 app.listen(5000);
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,80 @@
-'use strict';
-
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var nodemon = require('gulp-nodemon');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
-gulp.task('js',  function () {
-  return gulp.src('client/**/*.js')
-    // do stuff to JavaScript files
-    //.pipe(uglify())
-    //.pipe(gulp.dest('...'));
+var path = {
+  HTML: './client/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src/',
+  ENTRY_POINT: './client/js/app.js'
+};
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('css', function () {
-  return gulp.src('client/**/*.css')
-    .pipe(browserSync.reload({ stream: true }));
-})
-
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+gulp.task('copy-bower-components', function () {
+ gulp.src('./client/bower_components/**')
+ .pipe(gulp.dest(path.DEST + '/bower_components'));
 });
 
-gulp.task('default', ['browser-sync'], function () {
-  	gulp.watch('client/**/*.js',   ['js', browserSync.reload]);
-  	gulp.watch('client/**/*.css',  ['css']);
-	gulp.watch('client/**/*.html', ['bs-reload']);
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
-	browserSync.init(null, {
-		proxy: "http://localhost:5000",
-        files: ["..client/**/*.*"],
-        browser: "google chrome",
-        port: 7000,
-	});
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+    .pipe(gulp.dest(path.DEST_BUILD));
 });
-gulp.task('nodemon', function (cb) {
-	
-	var started = false;
-	
-	return nodemon({
-		script: 'app.js'
-	}).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		if (!started) {
-			cb();
-			started = true; 
-		} 
-	});
+
+gulp.task('replace-dev-html', function(){
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      'js': path.DEST_SRC + path.OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
 });
+
+gulp.task('replace-prod-html', function(){
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      'js': 'build/' + path.MINIFIED_OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('production', ['replace-prod-html', 'build', 'copy-bower-components']);
+
+gulp.task('default', ['copy-bower-components', 'replace-dev-html', 'watch']);
